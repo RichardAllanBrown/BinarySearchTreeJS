f6ee5cbc240f99ea0950c752dc81357b8cb39edd
Richard Brown <richard.allan.brown@gmail.com>
2013-08-07 16:38:05 +0100

    Added additional tests
    Added extra tests around deletions and checking the toArray method for the BTree.

--- BTreeTest.js
+++ BTreeTest.js
@@ -75,7 +75,14 @@ test.assertFalse(tree.add(7), "Cannot add same value again");
 /* Size, toArray and toString work as expected */
 test.assertEqual(5, tree.size(), "A different tree size is correctly identified.");
 test.assertEqual("1,3,5,7,10", tree.toString(), "toString method correctly prints out the tree.");
-test.assertEqual([1,3,5,7,10], tree.toArray(), "toArray correctly returns tree as an array.");
+
+var result = tree.toArray();
+test.assertEqual(5, result.length, "toArray returns the correct length array.");
+test.assertTrue(result.indexOf(1) > -1, "toArray returns array with 1 in it.");
+test.assertTrue(result.indexOf(3) > -1, "toArray returns array with 3 in it.");
+test.assertTrue(result.indexOf(5) > -1, "toArray returns array with 5 in it.");
+test.assertTrue(result.indexOf(7) > -1, "toArray returns array with 7 in it.");
+test.assertTrue(result.indexOf(10) > -1, "toArray returns array with 10 in it.");
 
 /* Tree can have elements removed from it */
 test.assertFalse(tree.remove(8), "Cannot remove object that does not exist.");
@@ -84,6 +91,12 @@ test.assertEqual(4, tree.size(), "Tree size updates correctly when leaf node is 
 
 test.assertTrue(tree.remove(10), "Tree updates when node with one child is deleted.");
 test.assertEqual(3, tree.size(), "Tree size correctly updates after node with one child deleted.");
+
+/* Can delete root element */
+var tree = new BinarySearchTree();
+tree.add(1);
+test.assertTrue(tree.remove(1), "Can remove root node of tree with 1 element");
+test.assertEqual(0, tree.size(), "Tree is empty when root node is only node and is removed");
 
 /* Creating a larger more complex tree */
 var tree = new BinarySearchTree();
@@ -97,12 +110,9 @@ tree.add(14);
 tree.add(12);
 tree.add(18);
 
-console.log(tree.toString());
-
 test.assertTrue(tree.remove(15), "Tree can perform a remove on a node with 2 children.");
 test.assertEqual(8, tree.size(), "Tree size is correct after complex delete.");
-
-console.log(tree.toString());
+test.assertEqual("3,5,7,10,12,14,17,18", tree.toString(), "Tree has correct nodes after complex delete.");
 
 /* Print test results */
 test.printResults();
